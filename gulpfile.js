8e2c85b8a2fd58031e44d44751cc76b3555770a9
Ryan Rho <rho@uber.com>
2015-02-16 02:10:48 -0800

    Set up Gulp JS.

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,20 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', ['build-sass']);
+gulp.task('default', ['build-sass', 'build-js']);
 
 gulp.task('build-sass', function buildSass() {
   gulp.src('./client/stylesheets/*.scss')
       .pipe(sass())
-      .pipe(gulp.dest('./public/style.css'));
+      .pipe(gulp.dest('./public'));
 });
 
-// TODO: Minify & Uglify JS
+gulp.task('build-js', function buildJs() {
+  gulp.src('./client/javascripts/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('./public'));
+});
 
 // TODO: 6-to-5 JS
