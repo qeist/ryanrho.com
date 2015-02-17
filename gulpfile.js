efb9aa5d66bf5e7fd6cf4dfcfaf9c91d47f12f96
Ryan Rho <rho@uber.com>
2015-02-16 17:37:01 -0800

    Gulp Browserify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,28 @@
 'use strict';
 
 var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 
-var SASS_PATH = './client/stylesheets/**/*.scss';
-var JS_PATH = './client/javascripts/**/*.js';
-
-gulp.task('default', ['build-sass', 'build-js']);
+gulp.task('default', ['build-sass', 'build-client-js']);
 
 gulp.task('build-sass', function buildSass() {
-  gulp.src(SASS_PATH)
-      .pipe(watch(SASS_PATH))
+  gulp.src('client/stylesheets/main.scss')
+      .pipe(watch('client/stylesheets/**/*.scss'))
       .pipe(sass())
+      .pipe(rename('style.css'))
       .pipe(gulp.dest('./public'));
 });
 
-gulp.task('build-js', function buildJs() {
-  gulp.src(JS_PATH)
-      .pipe(watch(JS_PATH))
+gulp.task('build-client-js', function buildJs() {
+  gulp.src('client/javascripts/main.js')
+      .pipe(browserify())
+      .pipe(rename('script.js'))
       .pipe(uglify())
       .pipe(gulp.dest('./public'));
 });
-
-// TODO: Set up Browserify
 
 // TODO: 6-to-5 JS
