08836d98b51db8bd1803926bbc175cf2c0a98eae
Ryan Rho <rho@uber.com>
2015-02-17 02:02:15 -0800

    Color scheme and basic web page layout.

--- client/javascripts/app/components/home.js
+++ client/javascripts/app/components/home.js
@@ -3,14 +3,44 @@
 let React = require('react');
 let r = require('r-dom');
 
+const links = [
+  {
+    class: 'fa fa-facebook',
+    link: 'https://fb.com/ryanrho'
+  },
+  {
+    class: 'fa fa-linkedin',
+    link: 'https://www.linkedin.com/in/ryanrho'
+  },
+  {
+    class: 'fa fa-github',
+    link: 'https://github.com/rhobot'
+  },
+  {
+    class: 'fa fa-envelope',
+    link: 'mailto:i@ryanrho.com'
+  }
+];
+
 module.exports = React.createClass({
 
   displayName: 'Home',
 
   render() {
-    return r.div({className: 'container'}, [
-      r.h1({className: 'text-center'}, 'Ryan Rho'),
-      r.i({className: 'fa fa-bed'})
-    ]);
+    return (
+      r.div({className: 'container'}, [
+        r.h1({className: 'text-center'}, 'Ryan Rho'),
+
+        r.ul({className: 'text-center list-unstyled list-inline'},
+          links.map(function renderLink(item) {
+            return (
+              r.a({href: item.link}, [
+                r.i({className: item.class})
+              ])
+            );
+          })
+        )
+      ])
+    );
   }
 });
