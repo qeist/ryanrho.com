8fae7d72341bdd785b46a185b9e53730b092b6f4
Ryan Rho <i@ryanrho.com>
2015-05-04 04:53:16 -0700

    let -> var.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.static(__dirname + '/public'));
 
 function getRoute(HtmlComponent, req, res) {
   routes(req.originalUrl, function render(Handler) {
-    let html = React.renderToStaticMarkup(r(ReactKNightHtml, {
+    var html = React.renderToStaticMarkup(r(ReactKNightHtml, {
       markup: React.renderToString(r(Handler))
     }));
 
