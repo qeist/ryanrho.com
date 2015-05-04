df86f217085be9517422a4c7c9cbf3330b0c46e2
Ryan Rho <i@ryanrho.com>
2015-05-04 04:48:49 -0700

    Do not use ES6 in node.

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 'use strict';
 
-import registerBabel from 'babel/register';
-registerBabel();
+require('babel/register');
 
-import React from 'react';
-import r from 'r-dom';
+var React = require('react');
+var r = require('r-dom');
 
-import ReactHtml from './client/javascripts/app/html.jsx';
-import ReactKNightHtml from './client/javascripts/k-night/html.jsx';
-import routes from './client/javascripts/routes.jsx';
+var ReactHtml = require('./client/javascripts/app/html.jsx');
+var ReactKNightHtml = require('./client/javascripts/k-night/html.jsx');
+var routes = require('./client/javascripts/routes.jsx');
 
-let express = require('express');
-let app = express();
+var express = require('express');
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 function getRoute(HtmlComponent, req, res) {
-  routes(req.originalUrl, (Handler) => {
+  routes(req.originalUrl, function render(Handler) {
     let html = React.renderToStaticMarkup(r(ReactKNightHtml, {
       markup: React.renderToString(r(Handler))
     }));
@@ -30,6 +29,6 @@ app.get('/', getRoute.bind(this, ReactHtml));
 app.get('/k-night', getRoute.bind(this, ReactKNightHtml));
 
 app.listen(app.get('port'), () => {
-  let port = app.get('port');
+  var port = app.get('port');
   console.log(`Node app is running at localhost: ${port}`);
 });
